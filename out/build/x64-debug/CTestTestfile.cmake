7e8eadd0a08a85e07b8c32590c9da594ea5a3492
manu-sankar <manusankar0601@gmail.com>
2025-07-08 12:47:37 +0530

    updated

---
+++ out/build/x64-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Administrator/Desktop/cppApps/cpp.Training/Class Work/day65
+# Build directory: C:/Users/Administrator/Desktop/cppApps/cpp.Training/Class Work/day65/out/build/x64-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_add01 "C:/Users/Administrator/Desktop/cppApps/cpp.Training/Class Work/day65/out/build/x64-debug/test_add01.exe")
+set_tests_properties(test_add01 PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Administrator/Desktop/cppApps/cpp.Training/Class Work/day65/CMakeLists.txt;29;add_test;C:/Users/Administrator/Desktop/cppApps/cpp.Training/Class Work/day65/CMakeLists.txt;33;add_calc_test;C:/Users/Administrator/Desktop/cppApps/cpp.Training/Class Work/day65/CMakeLists.txt;0;")
